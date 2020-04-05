a97ca025ef438f2c396fc564328825090dca5abc
sWallyx <mikelsmartinez@aol.com>
2020-04-05 18:15:20 +0100

    End new copy method

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 # Script main file, will copy certain folder and files into the HardDrive
 
 HARD_DRIVE=/Volumes/LaCie
-
-current_date=$(date +%m-%d-%Y)
+HARD_DRIVE_BACKUP=/Volumes/LaCie/CS
+CURRENT_DATE=$(date +%m-%d-%Y)
 
 # Get script directory
 SCRIPT_DIRECTORY="${0%/*}"
@@ -14,24 +14,9 @@ source "$SCRIPT_DIRECTORY"/utils.sh
 # shellcheck source=/dev/null
 source "$SCRIPT_DIRECTORY"/colors.sh
 
-# create_folder_with_date "$current_date"
+disk_space
 
-# copy_sites "$HARD_DRIVE" "$current_date"
-
-# copy_documents "$HARD_DRIVE" "$current_date"
-
-# copy_dropbox "$HARD_DRIVE" "$current_date"
-
-# copy_iCloud_CV "$HARD_DRIVE" "$current_date"
-
-# copy_iCloud_Billetes_Vuelo "$HARD_DRIVE" "$current_date"
-
-# copy_iCloud_Wallpapers "$HARD_DRIVE" "$current_date"
-
-# copy_iCloud_KAMON "$HARD_DRIVE" "$current_date"
-
-# copy_iCloud_Images "$HARD_DRIVE" "$current_date"
+create_folder_with_date
 
 check_for_hard_drive
 
-#disk_space
